a75d3a671b8649d9495c618717b9662e9ea88180
Simon_Staehli <simon.staehli@students.fhnw.ch>
2021-08-30 13:19:28 +0200

    Added doc to model.py
    Changed api.py

--- ml_api/api.py
+++ ml_api/api.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import request, jsonify
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 transformer = CustomTransformer()
@@ -23,51 +24,72 @@ def predict():
         try:
             data = transformer.fit_transform(X=data)
             print('---- Transformed Data Successfully ----')
-        except:
+        except NotImplementedError:
             return jsonify('Something went wrong with the Transformation.')
         # Predictions
         try:
             predictions = model.model_predict(X=data)
             print('---- Data fed to the model and predictions returned ----')
-        except:
+        except NotImplementedError:
             return jsonify('Something went wrong with Predictions.')
 
         return jsonify(predictions.tolist())
 
-@app.route('/score', methods=['GET'])
+@app.route('/score', methods=['POST'])
 def get_model_score():
-    if request.method == 'GET':
+    if request.method == 'POST':
         try:
             data = request.get_json()
-            X = pd.read_json(data['X'])
-            y = pd.read_json(data['y'])
+            X = pd.read_json(data['X']).to_numpy()
+            y = pd.read_json(data['y']).to_numpy()
             print('---- Data Collected ----')
-
-        except:
+        except ValueError:
             return jsonify('No Valid Input for Model.')
         try:
-            model_score = model.model_score(X=X, y=y)
+            model_score = model.model_score(X=y, y=X)
             print('---- Data fed to model and Score returned ----')
 
-        except:
+        except TypeError:
             return jsonify('Datatype not valid. Be Sure to input list in format: [X, y]')
 
         return jsonify(model_score)
 
+@app.route('/update_model', methods=['PUT'])
+def update_model():
+    if request.method == 'PUT':
+        try:
+            data = request.get_json()
+            model_params = data['params']
+            print('---- Data Collected ----')
+        except ValueError:
+            return jsonify('No Valid Input for Model.')
+        try:
+            model.update_model_params(new_params=model_params)
+            print('---- Model updated with new parameters. ----')
+        except TypeError:
+            return jsonify('Parameters not accepted.')
+
+        return jsonify('Parameters updated successfully.')
+
+@app.route('/delete_model', methods=['DELETE'])
+def delete_model():
+    if request.method == 'DELETE':
+        try:
+            model.delete_model()
+        except ValueError:
+            return jsonify('No Valid Input for Model.')
+
+        return jsonify('Model Deleted Successfully.')
 
 @app.route('/model_params', methods=['GET'])
-def score():
+def model_parameters():
     if request.method == 'GET':
         try:
             model_params = model.return_parameters()
-        except:
+        except TypeError:
             return jsonify('Datatype not valid. Be Sure to input list in format: [X, y]')
 
-        return model_params
-
-
-
-
+        return jsonify(model_params.tolist())
 
 
 if __name__ == '__main__':
--- ml_api/model.py
+++ ml_api/model.py
@@ -1,16 +1,25 @@
 import numpy as np
 import pandas as pd
 import pickle
+import os
 # Additional Imports
 import sklearn
 
 
 class CustomModel(object):
     """
-
+    Model Class for the ML-API
     """
 
     def __init__(self, filepath):
+        """
+
+        Parameters
+        ----------
+        filepath:
+            Path to your model. Preferably in pickle format.
+
+        """
         super(CustomModel, self).__init__()
         self.model = None
         self.filepath = filepath
@@ -18,17 +27,88 @@ class CustomModel(object):
 
     def _load_model(self):
         """
-
-        Returns
-        -------
-
+        This function is called at initialization of the class and loads
+        model as attribute to class
         """
         with open(self.filepath, 'rb') as my_model:
             self.model = pickle.load(my_model)
 
     def model_predict(self, X):
+        """
+        Returns predictions of the model to given X.
+
+        Parameters
+        ----------
+        X:
+            Data as numpy array or compatible with model.
+
+        Returns
+        -------
+        y_pred:
+            predictions according to X
+
+        """
         return self.model.predict(X=X)
 
     def model_score(self, X, y):
+        """
+        Returns the score of the model for given data.
+        This method is strongly related to the class of the model and which attributes it provides.
+
+        Parameters
+        ----------
+        X:
+            Predictor variables
+        y:
+            Target variable
+
+        Returns
+        -------
+        score:
+            score according to model description (mostly R2 or Accuracy)
+
+        """
         return self.model.score(X, y)
+
+    def return_parameters(self):
+        """
+        THis method is strongly related to the class attributes of the model.
+        i.E. LinearRegression class does not have same attributes as KNN class.
+
+        Returns
+        -------
+        model_coef:
+            Coefficients of the model.
+
+        """
+        try:
+            model_coef = self.model.coef_
+        except:
+            model_coef = 'Model does not have a .coef_ attribute.'
+
+        return model_coef
+
+    def delete_model(self):
+        """
+        Deletes the model in given filepath object.
+
+        Returns
+        -------
+        None
+        """
+        os.remove(self.filepath)
+        self.model = None
+
+    def update_model_params(self, new_params):
+        """
+        Replaces the old parameters of the model by new parameters.
+        This method is strongly related to the class object and needs to be adapted in advance.
+
+        Returns
+        -------
+        None
+        """
+        new_params = np.array(new_params)
+        self.model.coef_ = new_params
+
 
