97b5b358feb8bb283f8c4f47000e5b24ed881bbc
Simon_Staehli <simon.staehli@students.fhnw.ch>
2021-09-14 14:02:59 +0200

    change stream_exe.py to work with config.json

--- image_scraper/run/stream_exe.py
+++ image_scraper/run/stream_exe.py
@@ -1,14 +1,20 @@
 from image_scraper.imagescraper500 import ImageStream500
 from selenium import webdriver
+import json
 
 if __name__ == '__main__':
-    # This script can be added to the computers task schedulers
-    # Additionally this script can be individualized with parameters
+    # load configuration file
+    with open('config.json', 'rb') as json_file:
+        data = json.load(json_file)['scheduler']
+    
     print(10*'*', 'Start Process', 10*'*')
-    streaming = ImageStream500(webdriver=webdriver.Chrome('../src/chromedriver.exe'),
-                               popularity='fresh',
-                               iter_sampling_rate=5,
-                               batchsize=10,
-                               stream_time=5)
+    streaming = ImageStream500(
+        webdriver=webdriver.Chrome(data['webdriver_path']),
+        popularity=data['popularity'],
+        iter_sampling_rate=data['iter_sampling_rate'],
+        batchsize=data['batchsize'],
+        stream_time=data['stream_time']
+    )
     streaming.stream()
+    
     print(10*'*', 'End Process', 10*'*')
