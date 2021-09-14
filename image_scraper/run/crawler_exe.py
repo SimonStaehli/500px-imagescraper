0952fcaf81846c108f80b347484d57b29e42928a
Simon_Staehli <simon.staehli@students.fhnw.ch>
2021-09-14 13:45:54 +0200

    Changes to run files and adjusted config file + setup

--- image_scraper/run/crawler_exe.py
+++ image_scraper/run/crawler_exe.py
@@ -1,11 +1,17 @@
 from image_scraper.imagescraper500 import ImageCrawler500
 from selenium import webdriver
+import json
 
 if __name__ == '__main__':
+    # Load configuration file
+    with open('config.json', 'rb') as json_file:
+        data = json.load(json_file)['crawler']
+    
+    
     print(10*'*', 'Start Process', 10*'*')
-    crawler = ImageCrawler500(webdriver=webdriver.Chrome('../src/chromedriver.exe'),
-                              amount_per_class=100,
-                              popularity_ranking='popular'
+    crawler = ImageCrawler500(webdriver=webdriver.Chrome(data['webdriver_path']),
+                              amount_per_class=data['amount_per_class'],
+                              popularity_ranking=data['popularity_ranking']
                               )
     crawler.crawl()
     print(10*'*', 'End Process', 10*'*')
--- image_scraper/run/stream_exe_no_scheduler.py
+++ image_scraper/run/stream_exe_no_scheduler.py
@@ -1,18 +1,26 @@
 from image_scraper.imagescraper500 import ImageStream500
 from selenium import webdriver
 import datetime as dt
-
+import json
 
 if __name__ == '__main__':
-    running_times = ['10:01', '15:14', '20:53'] # <- put your preferred running time in here
-    duration_in_minutes = 15
+    # load config
+    with open('config.json', 'rb') as json_file:
+        data = json.load(json_file)['no_scheduler']
+    
+    # Map times to running times
+    running_times = list(data['running_times'].values())
+   
     while True:
         if dt.datetime.now().strftime('%H:%M') in running_times:
             print(10 * '*', f'Start Process at {dt.datetime.now().strftime("%H:%M:%S")}', 10 * '*')
-            streaming = ImageStream500(webdriver=webdriver.Chrome('../src/chromedriver.exe'),
-                                       popularity='upcoming',
-                                       iter_sampling_rate=10,
-                                       batchsize=20,
-                                       stream_time=duration_in_minutes)
+            streaming = ImageStream500(
+                webdriver=webdriver.Chrome(data['webdriver_path']),
+                popularity=data['popularity'],
+                iter_sampling_rate=data['iter_sampling_rate'],
+                batchsize=data['batchsize'],
+                stream_time=data['stream_time']
+            )
             streaming.stream()
+            
             print(10 * '*', f'End Process at {dt.datetime.now().strftime("%H:%M:%S")}', 10 * '*')
